db85c1eacb9db689509f07fd22235e982fa03b3c
ence88 <ence88@gmail.com>
2020-11-08 20:31:00 +0900

    'update'

--- publish.bat
+++ publish.bat
@@ -1,17 +1,13 @@
 echo 'deploying'
 
-rem commit source and push to github
 git add -A
 git commit -m 'update'
 git push origin
 
-rem build public folder
 hugo
 
-rem change to public directory
 cd public
 
-rem commit master and push to github
 git add -A
 git commit -m 'update'
 git push origin master
